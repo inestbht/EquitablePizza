e3e70a89ba325ac025840d2fb600e5118e807c79
Samuel Pena <inestbht@gmail.com>
2022-05-31 22:20:30 -0500

    COMPLETED Ch. 3.4 - EXERCISE 1 - Optionals (EquitablePizza)

--- EquitablePizza/ViewController.swift
+++ EquitablePizza/ViewController.swift
@@ -9,12 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var numberOfSlices: UITextField!
+    @IBOutlet weak var numberOfEaters: UITextField!
+    @IBOutlet weak var resultsLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        
+        guard let numberOfSlices = Double(numberOfSlices.text!) else {
+            resultsLabel.text = "Please enter a valid number of slices."
+            return
+        }
+        guard let numberOfEaters = Double(numberOfEaters.text!) else {
+            resultsLabel.text = "Please enter a valid number of eaters."
+            return
+        }
+        guard numberOfEaters != 0 else {
+            resultsLabel.text = "Cannot divide a pizza by zero."
+            return
+        }
+        
+        resultsLabel.text = "Each person gets \(numberOfSlices/numberOfEaters) slices."
+        
+        
+//        if let numberOfSlices = Double(numberOfSlices.text!) {
+//            if let numberOfEaters = Double(numberOfEaters.text!) {
+//                if numberOfEaters != 0 {
+//                    resultsLabel.text = "Each person get \(numberOfSlices/numberOfEaters) slices"
+//                } else {
+//                    resultsLabel.text = "Cannot divide pizza by zero."
+//                }
+//            } else {
+//                resultsLabel.text = "Please enter a valid number of eaters."
+//            }
+//        } else {
+//            resultsLabel.text = "Please enter a valid number of slices."
+//        }
+    }
 
 }
 
